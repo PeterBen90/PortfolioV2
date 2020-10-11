f7f1a7430b2d12ea5f47ab73cd289d0bb06e28e6
Peter Benvenuto <peterbenvenuto90@gamil.com>
2020-10-11 14:01:11 -0400

    updated landing page

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,4 +56,5 @@ function menuOnClick() {
 	document.getElementById("menu-bar").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg");
+	document.getElementById("menu-items").classList.toggle("hidden");
 }
