afdce1f4c1d43f70691c98a07f513e6c6577d997
Peter Benvenuto <peterbenvenuto90@gamil.com>
2020-10-11 14:49:10 -0400

    plz work

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,3 +58,10 @@ function menuOnClick() {
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 	document.getElementById("menu-items").classList.toggle("hidden");
 }
+
+const appHeight = () => {
+	const doc = document.documentElement;
+	doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
