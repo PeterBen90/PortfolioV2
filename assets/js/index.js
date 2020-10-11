8781852a567aacbdf34ecdbbc6c770ac0d1bd4b4
Peter Benvenuto <peterbenvenuto90@gamil.com>
2020-10-11 14:59:27 -0400

    not gonna work

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,9 +59,12 @@ function menuOnClick() {
 	document.getElementById("menu-items").classList.toggle("hidden");
 }
 
-const appHeight = () => {
-	const doc = document.documentElement;
-	doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-};
-window.addEventListener("resize", appHeight);
-appHeight();
+// const appHeight = () => {
+// 	const doc = document.documentElement;
+// 	doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+// };
+// window.addEventListener("resize", appHeight);
+// appHeight();
+
+var viewportHeight = $("#main").outerHeight();
+$("#main").css({ height: viewportHeight });
