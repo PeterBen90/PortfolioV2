59045954cca5d4bab3abd12a5801d32800d6a5ef
Peter Benvenuto <peterbenvenuto90@gamil.com>
2020-10-11 15:09:42 -0400

    plz work

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,9 +59,7 @@ function menuOnClick() {
 	document.getElementById("menu-items").classList.toggle("hidden");
 }
 
-const appHeight = () => {
-	const doc = document.documentElement;
-	doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-};
-window.addEventListener("resize", appHeight);
-appHeight();
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
