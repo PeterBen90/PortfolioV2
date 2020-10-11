0614c5fd4c3fa5601b2c289d161eaec22d0d814b
Peter Benvenuto <peterbenvenuto90@gamil.com>
2020-10-11 14:42:50 -0400

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,19 +58,3 @@ function menuOnClick() {
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 	document.getElementById("menu-items").classList.toggle("hidden");
 }
-
-function setDocHeight() {
-	document.documentElement.style.setProperty(
-		"--vh",
-		`${window.innerHeight / 100}px`
-	);
-}
-
-window.addEventListener("resize", function () {
-	setDocHeight();
-});
-window.addEventListener("orientationchange", function () {
-	setDocHeight();
-});
-
-setDocHeight();
